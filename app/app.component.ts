96ddfeb70a6f2eee588523ebe7592b01aa87e6d0
Chris Martinez <cardamomclouds@yahoo.com>
2016-10-21 08:48:39 -0700

    creat calorie class and display results to screen

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { Item }   from './item.model';
+
 
 @Component({
   selector: 'my-app',
   template: `
   <h1>My First Angular 2 App</h1>
+  well this loads
+  <div *ngFor="let item of masterItemList"class="">
+      <p><span>Name: {{item.name}} </span></p>
+      <p><span>Calories: {{item.calories}}</span></p>
+      <p><span>Details: {{item.details}}</span></p>
+  </div>
+
   `
 })
 
 export class AppComponent {
-
+    public masterItemList: Item[] = [
+    new Item("hamburger", 500, "delicious"),
+    new Item("Magnum Bar", 600, "oops"),
+    new Item("Al pasto Tacos", 400, "yaas")
+    ]
 }
